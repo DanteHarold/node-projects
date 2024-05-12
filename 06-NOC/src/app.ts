cfad0d370a81100796e35ee5bc1e3ad0f1c293da
DanteHarold <novianrelay@gmail.com>
2024-05-11 22:12:56 -0500

    Final - Clean Architecture - Repository Pattern

--- 06-NOC/src/app.ts
+++ 06-NOC/src/app.ts
@@ -1,12 +1,9 @@
-import "dotenv/config";
 import { Server } from "./presentation/server";
-import { envs } from "./config/plugins/envs.plugin";
 
-async () => {
+(async () => {
   main();
-};
+})();
 
-async function main() {
-  // Server.start();
-  console.log(envs);
+function main() {
+  Server.start();
 }
